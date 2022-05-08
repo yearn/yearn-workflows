c649de5eaee5162c1b4d7fcd15260beaf0d6b0ab
kx9x <kx9x@protonmail.com>
2022-05-07 19:37:58 -0700

    fix: entrypoint symlink to compilers and do proper exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+ln -s /root/.solcx /github/home/.solcx
+ln -s /root/.vvm /github/home/.vvm
 
 pip install -r requirements-dev.txt
 /root/.local/bin/brownie
 cp network-config.yaml ~/.brownie/network-config.yaml
 /root/.local/bin/brownie networks list true
 brownie compile
-python3 -m multisig_ci brownie run $1 $2 --network $3-main-fork 1>output.txt 2>error.txt || EXIT_CODE=$?
+python3 -m multisig_ci brownie run $1 $2 --network $3-main-fork 1>output.txt 2>error.txt
+EXIT_CODE=$?
 echo "::set-output name=brownie-exit-code::$EXIT_CODE"
 
 echo "::group:: Output"
@@ -18,13 +21,14 @@ echo "::endgroup::"
 
 echo "::set-output name=nonce::$NONCE"
 echo "::set-output name=safe_link::$SAFE_LINK"
-echo $GITHUB_ACTION_SEND
+echo "Action send is $GITHUB_ACTION_SEND"
+echo "exit code is $EXIT_CODE"
 
 if [[ "$GITHUB_ACTION_SEND" == "true" && "$NONCE" == "" ]]
 then
     echo "::set-output name=error-reason::'failed to find nonce'"
     exit 1
-elif [[ "$GITHUB_ACTION_SEND" == "true" && "$SAFE_LINK" -eq "" ]]
+elif [[ "$GITHUB_ACTION_SEND" == "true" && "$SAFE_LINK" == "" ]]
 then
     echo "::set-output name=error-reason::'failed to find safe link'"
     exit 1
