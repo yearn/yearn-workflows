c20d02a74a16024e7e494a1f5136324cbfb077e9
kx9x <kx9x@protonmail.com>
2022-05-15 22:16:27 -0700

    remove chown

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,9 @@
 export PATH=/home/pn/.local/bin:${PATH}
 export PATH=/github/home/.local/bin:${PATH}
 
-sudo chown -R 1000:1000 /github/workspace
-sudo chown -R 1000:1000 /github/home
-sudo chown -R 1000:1000 /github/file_commands
-#sudo chown 1000:1000 /home/pn/.local/lib/python3.9/site-packages/
+# sudo chown -R 1000:1000 /github/workspace
+# sudo chown -R 1000:1000 /github/home
+# sudo chown -R 1000:1000 /github/file_commands
 
 ln -s /home/pn/.solcx /github/home/.solcx
 ln -s /home/pn/.vvm /github/home/.vvm
@@ -15,7 +14,6 @@ cp -r /home/pn/.local/bin/ /github/home/.local/
 ls -l /github/home/.local/bin/
 ls -l /home/pn/.local/bin/
 
-#pip install -v -r requirements-dev.txt
 VIRTUAL_ENV=/home/pn/.local/pipx/venvs
 python3 -m venv $VIRTUAL_ENV
 PATH="$VIRTUAL_ENV/bin:$PATH"
