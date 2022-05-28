5d05893de7f250333f6a42e0d34646b9c4c492de
kx9x <kx9x@protonmail.com>
2022-05-27 19:47:24 -0700

    download a thicc deployments.db

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,6 +26,8 @@ PATH="$VIRTUAL_ENV/bin:$PATH"
 
 mkdir ~/.brownie
 cp network-config.yaml ~/.brownie/network-config.yaml
+cp deployments.db ~/.brownie/deployments.db
+
 python3 -c "import site;print([p for p in site.getsitepackages() if p.endswith(('site-packages', 'dist-packages')) ][0])"
 python3 -c "import sys; print(sys.prefix)"
 python3 -m multisig_ci brownie run $1 $2 --network $3-main-fork 1>output.txt 2>error.txt || EXIT_CODE=$?
