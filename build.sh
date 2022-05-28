8ad876d53ef0b5748136a988d6a07d22d80b1c04
kx9x <kx9x@protonmail.com>
2022-05-27 22:06:27 -0700

    fix: tag

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build -t robowoofy:v0.0.1 --network=host -f build.Dockerfile .
 az acr login --name robowoofyregistry
-docker tag robowoofy:v0.0.1 robowoofyregistry.azurecr.io/robowoofy
-docker push robowoofyregistry.azurecr.io/robowoofy
+docker tag robowoofy:v0.0.1 robowoofyregistry.azurecr.io/robowoofy:v0.0.1
+docker push robowoofyregistry.azurecr.io/robowoofy:v0.0.1
