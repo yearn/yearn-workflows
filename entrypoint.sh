7246452a050f16776988ef357858b3c710ebff0d
kx9x <kx9x@protonmail.com>
2022-05-28 16:03:16 -0700

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,24 +31,30 @@ fi
 python3 -m multisig_ci brownie run $1 $2 --network $3-main-fork
 EXIT_CODE=$?
 echo "::set-output name=brownie-exit-code::$EXIT_CODE"
+
+echo "cat nonce"
 cat $HOME/nonce.txt
+echo "cat safe"
 cat $HOME/safe.txt
+
 NONCE=$(cat $HOME/nonce.txt)
 SAFE_LINK=$(cat $HOME/safe.txt)
 
+echo "nonce is $NONCE"
+echo "safe link is $SAFE_LINK"
+
 echo "::set-output name=nonce::$NONCE"
 echo "::set-output name=safe_link::$SAFE_LINK"
+
 echo "github action send is $GITHUB_ACTION_SEND"
 echo "nonce is $NONCE"
 echo "safe link is $SAFE_LINK"
 echo "exit code is $EXIT_CODE"
 
-if [[ "$GITHUB_ACTION_SEND" == "true" && "$NONCE" == "" ]]
-then
+if [[ "$GITHUB_ACTION_SEND" == "true" && "$NONCE" == "" ]]; then
     echo "::set-output name=error-reason::'failed to find nonce'"
     exit 1
-elif [[ "$GITHUB_ACTION_SEND" == "true" && "$SAFE_LINK" == "" ]]
-then
+elif [[ "$GITHUB_ACTION_SEND" == "true" && "$SAFE_LINK" -eq "" ]]; then
     echo "::set-output name=error-reason::'failed to find safe link'"
     exit 1
 else
