cf6070fd0c0399a50c9dee8b831d079d2afbb7a8
kx9x <kx9x@protonmail.com>
2022-05-28 15:22:16 -0700

    fix shit

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build -t robowoofy:v0.0.5 --network=host -f build.Dockerfile .
+docker build -t robowoofy:v0.0.6 --network=host -f build.Dockerfile .
 az acr login --name robowoofyregistry
-docker tag robowoofy:v0.0.5 robowoofyregistry.azurecr.io/robowoofy:v0.0.5
-docker push robowoofyregistry.azurecr.io/robowoofy:v0.0.5
+docker tag robowoofy:v0.0.6 robowoofyregistry.azurecr.io/robowoofy:v0.0.6
+docker push robowoofyregistry.azurecr.io/robowoofy:v0.0.6
