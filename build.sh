9bf91183037b909eb3c1d029971281af5da35313
kx9x <kx9x@protonmail.com>
2022-05-29 13:56:36 -0700

    v0.0.11

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build -t robowoofy:v0.0.10 --network=host -f build.Dockerfile .
+docker build -t robowoofy:v0.0.11 --network=host -f build.Dockerfile .
 az acr login --name robowoofyregistry
-docker tag robowoofy:v0.0.10 robowoofyregistry.azurecr.io/robowoofy:v0.0.10
-docker push robowoofyregistry.azurecr.io/robowoofy:v0.0.10
+docker tag robowoofy:v0.0.11 robowoofyregistry.azurecr.io/robowoofy:v0.0.11
+docker push robowoofyregistry.azurecr.io/robowoofy:v0.0.11
