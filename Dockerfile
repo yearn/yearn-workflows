e66800e36e36cfcd22769779e544a416b58a810f
kx9x <kx9x@protonmail.com>
2023-05-18 11:48:36 -0700

    fix: get rid of deprecated set-output and replace concurrency with built-in method

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
-FROM robowoofyregistry.azurecr.io/robowoofy:use-weiroll-0405940763dc622626d58c874358e23e4d739b25
+FROM robowoofyregistry.azurecr.io/robowoofy:use-weiroll-aefe2350f2f74947cceca6147d9de8c256b1e57f
 ENTRYPOINT [ "/home/pn/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ else
 fi
 
 python3 -m multisig_ci brownie run $1 $2 --network $3-main-fork 1>output.txt 2>error.txt || EXIT_CODE=$?
-echo "::set-output name=brownie-exit-code::$EXIT_CODE"
+echo "brownie-exit-code=$EXIT_CODE" >> $GITHUB_OUTPUT
 echo "::group:: Output"
 cat output.txt
 echo "::endgroup::"
@@ -40,14 +40,14 @@ echo "::endgroup::"
 NONCE=$(cat $HOME/nonce.txt)
 SAFE_LINK=$(cat $HOME/safe.txt)
 
-echo "::set-output name=nonce::$NONCE"
-echo "::set-output name=safe_link::$SAFE_LINK"
+echo "nonce=$NONCE" >> $GITHUB_OUTPUT
+echo "safe_link=$SAFE_LINK" >> $GITHUB_OUTPUT
 
 if [[ "$GITHUB_ACTION_SEND" == "true" && "$NONCE" == "" ]]; then
-    echo "::set-output name=error-reason::'failed to find nonce'"
+    echo "error-reason='failed to find nonce'" >> $GITHUB_OUTPUT
     exit 1
 elif [[ "$GITHUB_ACTION_SEND" == "true" && "$SAFE_LINK" -eq "" ]]; then
-    echo "::set-output name=error-reason::'failed to find safe link'"
+    echo "error-reason='failed to find safe link'" >> $GITHUB_OUTPUT
     exit 1
 else
     echo "Exiting with $EXIT_CODE"
