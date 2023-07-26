20c5da3a9fcacd094fbf07a6b2a1981056527062
kx9x <kx9x@protonmail.com>
2023-07-25 22:07:58 -0700

    fix: 3.9

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build -t robowoofy:weiroll --network=host -f build.Dockerfile .
+docker build -t robowoofy:upgrade --network=host -f build.Dockerfile .
 az acr login --name robowoofyregistry
-docker tag robowoofy:weiroll robowoofyregistry.azurecr.io/robowoofy:weiroll
-docker push robowoofyregistry.azurecr.io/robowoofy:weiroll
+docker tag robowoofy:upgrade robowoofyregistry.azurecr.io/robowoofy:upgrade
+docker push robowoofyregistry.azurecr.io/robowoofy:upgrade
